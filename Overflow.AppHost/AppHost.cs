175f14cea8a22db6e7a9b35b1fafcf7cd1f98a8b
João Santos <joao.santos@vfxplc.com>
2026-01-13 15:00:04 +0000

    Commit Db Connection and Migration
    Commit Db Connection and Migration

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,3 +1,4 @@
+using Aspire.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -7,8 +8,17 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
     .WithEnvironment("KC_BOOTSTRAP_ADMIN_USERNAME", "admin")
     .WithEnvironment("KC_BOOTSTRAP_ADMIN_PASSWORD", "admin");
 
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
+
 var questionService = builder.AddProject<QuestionService>("question-svc")
     .WithReference(keycloak)
-    .WaitFor(keycloak);
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb);
 
 builder.Build().Run();
--- QuestionService/Program.cs
+++ QuestionService/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,8 @@ builder.Services.AddAuthentication()
         options.Audience = "overflow";
     });
 
+builder.AddNpgsqlDbContext<QuestionDbContext>("questionDb");
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -22,10 +27,23 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
 app.MapControllers();
 
 app.MapDefaultEndpoints();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<QuestionDbContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception e)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(e, "An error occurred while migrating or seeding the database.");
+}
 
 app.Run();
