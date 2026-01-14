9fe111a35a2846f4d66d38a8398ac4534974c4ee
João Santos <joamsantos@sapo.pt>
2026-01-14 23:01:46 +0000

    Typesense (Search), RabbitMQ (Messaging Broker) and WolverineFX (Standard Messaging)
    Typesense (Search), RabbitMQ (Messaging Broker) and WolverineFX (Standard Messaging)

--- Overflow.AppHost/AppHost.cs
+++ Overflow.AppHost/AppHost.cs
@@ -1,4 +1,3 @@
-using Aspire.Hosting;
 using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -10,15 +9,38 @@ var keycloak = builder.AddKeycloak("keycloak", 6001)
 
 var postgres = builder.AddPostgres("postgres", port: 5432)
     .WithDataVolume("postgres-data")
-    .WithEnvironment("POSTGRES_PASSWORD", "postgres")
+    .WithEnvironment("POSTGRES_USER", "postgres")
+    .WithEnvironment("POSTGRES_PASSWORD", "XjGQt{E05N54xB4xp}RBk1")
     .WithPgAdmin();
 
+var typesenseApiKey = builder.AddParameter("typesense-api-key", secret: true);
+
+var typesense = builder.AddContainer("typesense", "typesense/typesense", "29.0")
+    .WithArgs("--data-dir", "/data", "--api-key", typesenseApiKey, "--enable-cors")
+    .WithVolume("typesense-data", "/data")
+    .WithHttpEndpoint(8108, 8108, name: "typesense");
+
+var typesenseContainer = typesense.GetEndpoint("typesense");
+
 var questionDb = postgres.AddDatabase("questionDb");
+
+var rabbitmq = builder.AddRabbitMQ("messaging")
+    .WithDataVolume("rabbitmq-data")
+    .WithManagementPlugin(port: 15672);
 
 var questionService = builder.AddProject<QuestionService>("question-svc")
     .WithReference(keycloak)
     .WithReference(questionDb)
+    .WithReference(rabbitmq)
     .WaitFor(keycloak)
-    .WaitFor(questionDb);
+    .WaitFor(questionDb)
+    .WaitFor(rabbitmq);
+
+var searchService = builder.AddProject<SearchService>("search-svc")
+    .WithEnvironment("typesense-api-key", typesenseApiKey)
+    .WithReference(typesenseContainer)
+    .WithReference(rabbitmq)
+    .WaitFor(typesense)
+    .WaitFor(rabbitmq);
 
 builder.Build().Run();
---
+++ QuestionService/Controllers/TagsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using QuestionService.Data;
+using QuestionService.Models;
+
+namespace QuestionService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TagsController(QuestionDbContext db) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Tag>>> GetTags() 
+        => await db.Tags.OrderBy(t => t.Name).ToListAsync();
+}
